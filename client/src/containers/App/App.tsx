c5da139c50a21999fdc4118b0806a4e5e0689696
Jakub Melzacki <melzacki.jakub@gmail.com>
2022-02-27 01:06:47 +0100

    Create react app

---
+++ client/src/containers/App/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body className="App-body">
+      </body>
+    </div>
+  );
+}
+
+export default App;
